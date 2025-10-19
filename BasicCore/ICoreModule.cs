a20e91f4504cb990ebf6743cc48299111acc77b2
Михаил <misha13022008@gmail.com>
2025-10-19 18:22:15 +0300

    clean up

--- BasicCore/ICoreModule.cs
+++ BasicCore/ICoreModule.cs
@@ -1,5 +1,6 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 namespace BasicCore;
 
 public interface ICoreModule
