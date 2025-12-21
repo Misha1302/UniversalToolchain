06002c084216104b5e3a9cce23f0cb3b80064974
Михаил <misha13022008@gmail.com>
2025-12-21 18:30:10 +0300

    Update simple_to_one_file.sh

--- UniversalToolchain/simple_to_one_file.sh
+++ UniversalToolchain/simple_to_one_file.sh
@@ -1,1 +1,1 @@
-python3 script_to_one_file.py --pattern "\.cs|\.csproj|\.sln" --root . --exclude-pattern ".*?Tests.*?|.*?bin.*?|.*?obj.*?" --output text.txt
+python3 script_to_one_file.py --pattern "\.cs" --root . --exclude-pattern ".*?Tests.*?|.*?bin.*?|.*?obj.*?" --output text.txt
