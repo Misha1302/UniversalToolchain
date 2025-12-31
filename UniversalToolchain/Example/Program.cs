a07c03642b90e942bc6dbf4d1b043731f75ea41e
Михаил <misha13022008@gmail.com>
2026-01-01 01:19:19 +0300

    DI for example

--- UniversalToolchain/Example/Program.cs
+++ UniversalToolchain/Example/Program.cs
@@ -1,49 +1,17 @@
-using System.Diagnostics;
-using System.Reflection.Emit;
-using AbstractIrConverters;
-using BasicCilCompiler;
-using BasicStdLib;
-using BytecodeDynamicMethodsCompiler;
-using ConditionsModule;
-using EqualityModule;
-using IdentifierModule;
-using LabelsModule;
-using LocalVariablesOptimizerModule;
-using SemicolonAsNewLineModule;
-using VariablesModule;
-
-var core = new BasicCoreImpl<DynamicMethod>(
-    () => new BasicLexerImpl(),
-    () => new BasicParserImpl(),
-    () => new BasicAstToBytecodeTranslatorImpl(),
-    () => new BytecodeToAbstractIrConverterImpl(),
-    () => new AbstractMethodsCompilerImpl(),
-    () => new DynamicMethodExecutor(),
-    [
-        new IdentifierModuleImpl(),
-        new ScopesModuleImpl(),
-        new NumbersModuleImpl(),
-        new WhitespaceModuleImpl(),
-        new SemicolonAsNewLineModuleImpl(),
-        new ArithmeticModuleImpl(),
-        new CSharpInteropModuleImpl(),
-        new LabelsModuleImpl(),
-        new VariablesModuleImpl(),
-        new EqualityModuleImpl(),
-        new ConditionsModuleImpl(),
-        new ComparisonOperations(),
-        new BooleanOperations(),
-        new LocalVariablesOptimizer(),
-        new ExecutorDebugLoggerImpl(),
-        new ParserConfigurationModuleImpl(ActionType.DumpConfiguration),
-        new LexerConfigurationModuleImpl(ActionType.DumpConfiguration)
-    ],
-    []
-);
-
 Main.LoadStdLibToThisAssembly();
 
-core.PrepareToRun(
+// Создаем DI контейнер
+var serviceCollection = new ServiceCollection();
+
+// Регистрируем все сервисы и модули
+ConfigureServices(serviceCollection);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+// Получаем ядро через DI
+var core = serviceProvider.GetRequiredService<ICoreRunnable>();
+
+var result = core.Run(
     """
     let sum = 0
     let i = 1
@@ -57,9 +25,59 @@ core.PrepareToRun(
     """
 );
 
-var sw = Stopwatch.StartNew();
-var result = core.RunPrepared();
-Console.WriteLine(sw.ElapsedMilliseconds);
-
-
 Console.WriteLine(result);
+return;
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    // Основные фабрики
+    services.AddSingleton<Func<ILexer>>(_ => () => new BasicLexerImpl());
+    services.AddSingleton<Func<IParser>>(_ => () => new BasicParserImpl());
+    services.AddSingleton<Func<IAstToBytecodeTranslator>>(_ => () => new BasicAstToBytecodeTranslatorImpl());
+    services.AddSingleton<Func<IAbstractMethodsTranslator>>(_ => () => new BytecodeToAbstractIrConverterImpl());
+    services.AddSingleton<Func<IExecutor<DynamicMethod>>>(_ => () => new DynamicMethodExecutor());
+
+    // Компиляторы
+    services.AddSingleton<AbstractMethodsCompilerImpl>();
+
+    // Все стандартные фронтенд-модули
+    services.AddSingleton<IFrontendCoreModule, IdentifierModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, ScopesModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, NumbersModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, WhitespaceModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, SemicolonAsNewLineModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, ArithmeticModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, CSharpInteropModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, LabelsModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, VariablesModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, EqualityModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, ConditionsModuleImpl>();
+    services.AddSingleton<IFrontendCoreModule, ComparisonOperations>();
+    services.AddSingleton<IFrontendCoreModule, BooleanOperations>();
+
+    // Дополнительные модули из примера
+    services.AddSingleton<IFrontendCoreModule, LocalVariablesOptimizer>();
+    services.AddSingleton<IFrontendCoreModule, ExecutorDebugLoggerImpl>();
+    services.AddSingleton<IFrontendCoreModule>(_ =>
+        new ParserConfigurationModuleImpl(ActionType.DumpConfiguration));
+    services.AddSingleton<IFrontendCoreModule>(_ =>
+        new LexerConfigurationModuleImpl(ActionType.DumpConfiguration));
+
+    // Регистрируем BasicCoreImpl<DynamicMethod> как основной ICoreRunnable
+    services.AddSingleton<ICoreRunnable>(provider =>
+    {
+        var modules = provider.GetServices<IFrontendCoreModule>().ToList();
+
+        return new BasicCoreImpl<DynamicMethod>(
+            provider.GetRequiredService<Func<ILexer>>(),
+            provider.GetRequiredService<Func<IParser>>(),
+            provider.GetRequiredService<Func<IAstToBytecodeTranslator>>(),
+            provider.GetRequiredService<Func<IAbstractMethodsTranslator>>(),
+            () => provider.GetRequiredService<AbstractMethodsCompilerImpl>(),
+            provider.GetRequiredService<Func<IExecutor<DynamicMethod>>>(),
+            modules,
+            [] // Middle-end modules
+        );
+    });
+}
