a5d09487709b6a34753f1cb1ee87c53187763745
Михаил <misha13022008@gmail.com>
2026-01-04 16:52:11 +0300

    fixed DynamicMethodExecutor

--- UniversalToolchain/BasicCilCompiler/DynamicMethodExecutor.cs
+++ UniversalToolchain/BasicCilCompiler/DynamicMethodExecutor.cs
@@ -7,11 +7,11 @@ namespace BasicCilCompiler;
 public class DynamicMethodExecutor : IExecutor<DynamicMethod>
 {
     private readonly Dictionary<DynamicMethod, DynamicMethodInvoker<object>> _cache = [];
-    private DynamicMethodInvoker<object> _q;
 
     public object Execute(DynamicMethod compilation)
     {
-        if (_q == null) _q = new DynamicMethodInvoker<object>(compilation);
-        return _q.Invoke();
+        if (!_cache.TryGetValue(compilation, out var del))
+            _cache.Add(compilation, del = new DynamicMethodInvoker<object>(compilation));
+        return del.Invoke();
     }
 }
