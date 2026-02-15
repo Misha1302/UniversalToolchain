8fd87ae92115af2162a12804e82e4c5225cd2a2d
Михаил <misha13022008@gmail.com>
2026-02-15 20:59:08 +0300

    Fix core factory registration and make extensible enum thread-safe

---
+++ UniversalToolchain/Tests/DependencyInjectionRegistrationTests.cs
@@ -0,0 +1,26 @@
+using BasicCore;
+using DependencyInjection;
+using IntermediateRepresentationAbstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection.Emit;
+
+namespace Tests;
+
+[TestFixture]
+public class DependencyInjectionRegistrationTests
+{
+    [Test]
+    public void AddWistServices_RegistersExecutableGiversForDynamicMethodAndAbstractIr()
+    {
+        var services = new ServiceCollection();
+        services.AddWistServices();
+
+        using var provider = services.BuildServiceProvider();
+
+        var dynamicMethodExecutableGivers = provider.GetServices<IExecutableGiver<DynamicMethod>>().ToList();
+        var abstractIrExecutableGivers = provider.GetServices<IExecutableGiver<IAbstractIR>>().ToList();
+
+        Assert.That(dynamicMethodExecutableGivers, Is.Not.Empty);
+        Assert.That(abstractIrExecutableGivers, Is.Not.Empty);
+    }
+}
