c5ef99d63c7b8218044be6c5065857166fddf7ef
Михаил <misha13022008@gmail.com>
2026-02-17 11:09:59 +0300

    Revert "clean up", 'cause ifs optimizations is slowing the app down
    This reverts commit 344198160c6dfbeb688dd901049bda04fc4cee02.

--- UniversalToolchain/LocalVariablesOptimizerModule/LocalVariablesOptimizer.cs
+++ UniversalToolchain/LocalVariablesOptimizerModule/LocalVariablesOptimizer.cs
@@ -196,10 +196,8 @@ public class LocalVariablesOptimizer : IIRProcessingModule
 
         var labelToIndex = new Dictionary<object, int>();
         for (var i = 0; i < instructions.Count; i++)
-        {
             if (IsLabel(instructions[i], out var label))
                 labelToIndex[label] = i;
-        }
 
         var branchTargets = CollectBranchTargets(instructions, labelToIndex);
         var hasBackwardBranches = HasBackwardBranch(instructions, labelToIndex);
@@ -340,10 +338,8 @@ public class LocalVariablesOptimizer : IIRProcessingModule
         for (var i = 0; i < instructions.Count; i++)
         {
             foreach (var target in ExtractBranchTargets(instructions[i]))
-            {
                 if (labelToIndex.TryGetValue(target, out var index))
                     result.Add(index);
-            }
         }
 
         return result;
@@ -367,15 +363,21 @@ public class LocalVariablesOptimizer : IIRProcessingModule
     {
         if ((instruction.UOpCode == UOpCode.Jmp || instruction.UOpCode == UOpCode.JmpIf || instruction.UOpCode == UOpCode.JmpIfNot) &&
             instruction.Operands.Count > 0)
+        {
             yield return instruction.Operands[0];
+        }
 
         if (instruction.UOpCode == UOpCode.Intrinsic && instruction.Operands.Count > 1 && instruction.Operands[0] is string name)
         {
             if (name == "switch" && instruction.Operands[1] is IEnumerable<object> labels)
+            {
                 foreach (var label in labels)
                     yield return label;
+            }
             else if ((name == "leave" || name.StartsWith("br", StringComparison.Ordinal)) && instruction.Operands[1] is not null)
+            {
                 yield return instruction.Operands[1];
+            }
         }
     }
 
