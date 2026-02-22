80582ca47aef66365f49be6476349cf06859bbb8
Михаил <misha13022008@gmail.com>
2026-02-23 00:21:28 +0300

    loops tests added v4

--- UniversalToolchain/Tests/Core/ErrorCasesTests.cs
+++ UniversalToolchain/Tests/Core/ErrorCasesTests.cs
@@ -8,14 +8,64 @@ public class ErrorCasesTests : TestBase
     {
         var code = "let 123 = 456";
 
-        try
-        {
-            ExecuteCode(code);
-            Assert.Fail();
-        }
-        catch
-        {
-            Assert.Pass();
-        }
+        Assert.That(() => ExecuteCode(code), Throws.Exception.With.Message.Not.Empty);
+    }
+
+    [Test]
+    public void Execute_ForLoopWithoutBodyScope_ThrowsException()
+    {
+        var code = @"
+                let sum = 0
+
+                for (let i = 1) (i <= 3) (i = i + 1)
+
+                sum
+            ";
+
+        Assert.That(() => ExecuteCode(code), Throws.Exception.With.Message.Not.Empty);
+    }
+
+    [Test]
+    public void Execute_WhileLoopWithoutConditionOrBody_ThrowsException()
+    {
+        var code = @"
+                let i = 0
+                while
+                i
+            ";
+
+        Assert.That(() => ExecuteCode(code), Throws.Exception.With.Message.Not.Empty);
+    }
+
+    [Test]
+    public void Execute_LoopWithSwappedBracketsAroundSections_ThrowsException()
+    {
+        var code = @"
+                let sum = 0
+
+                for ((let i = 1) (i <= 3)) (i = i + 1) (
+                    sum = sum + i
+                )
+
+                sum
+            ";
+
+        Assert.That(() => ExecuteCode(code), Throws.Exception.With.Message.Not.Empty);
+    }
+
+    [Test]
+    public void Execute_ForLoopWithWrongSectionOrder_ThrowsException()
+    {
+        var code = @"
+                let sum = 0
+
+                for (i <= 3) (let i = 1) (i = i + 1) (
+                    sum = sum + i
+                )
+
+                sum
+            ";
+
+        Assert.That(() => ExecuteCode(code), Throws.Exception.With.Message.Not.Empty);
     }
 }
