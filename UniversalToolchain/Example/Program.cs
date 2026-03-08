735cd30d57e66fef01c419e9e86663d9b082ac3a
Михаил <misha13022008@gmail.com>
2026-03-08 18:58:00 +0300

    bug example

--- UniversalToolchain/Example/Program.cs
+++ UniversalToolchain/Example/Program.cs
@@ -12,23 +12,23 @@ var provider = services.BuildServiceProvider();
 
 const string code =
     """
-    i = 5
-    while i < 25 ( i = i * 2 )
-    i
+    System.Console.WriteLine((if (a < 5 or b > 7) 5 else 3))
     """;
 
 
-// INTERPRETER
-var core = provider.GetService<ICoreRunnable>().NotNull();
-Console.WriteLine("Runned: " + core.Run(code));
+// // INTERPRETER
+// var core = provider.GetService<ICoreRunnable>().NotNull();
+// Console.WriteLine("Runned: " + core.Run(code, new Dictionary<string, object>
+// {
+//     ["a"] = 5,
+//     ["b"] = 7
+// }));
 
 
 
-/*
 // COMPILER
 var core = provider.GetService<IExecutableGiver<DynamicMethod>>().NotNull();
 var method = core.GetExecutable(code, new());
 var fastCallable = new DynamicMethodInvoker<int, int, int>(method);
 
 Console.WriteLine("Result: " + fastCallable.Invoke(7, 15));
-*/
