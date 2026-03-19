66fd64f3c64d43bdf0678d149a7ef25417a3924c
Михаил <misha13022008@gmail.com>
2026-03-19 20:33:16 +0300

    example added

--- UniversalToolchain/Example/Program.cs
+++ UniversalToolchain/Example/Program.cs
@@ -1,29 +1,18 @@
+using UniversalToolchain.Dialects.Wist;
+
 var services = new ServiceCollection();
+services.AddWistDialectServices();
 
-services.AddWistServices(options =>
-    options.ArithmeticMode = WistOptions.ArithmeticModeEnum.Native
-);
+using var provider = services.BuildServiceProvider();
+var workflow = provider.GetRequiredService<WistDialectExecutionWorkflow>();
 
-services.AddSingleton<IFrontendCoreModule>(new ExecutorDebugLoggerImpl());
-services.AddSingleton<IFrontendCoreModule>(new ParserConfigurationModuleImpl(ActionType.DumpConfiguration));
-services.AddSingleton<IFrontendCoreModule>(new LexerConfigurationModuleImpl(ActionType.DumpConfiguration));
-
-var provider = services.BuildServiceProvider();
-
-const string code =
-    """
-    System.Console.WriteLine((if (a < 5 or b > 7) 5 else 3))
-    0
-    """;
-
-
-// COMPILER
-var core = provider.GetService<IExecutableGiver<DynamicMethod>>().NotNull();
-var method = core.GetExecutable(code, new OrderedDictionary<string, Type>
+var dialect = workflow.ComposeFile("./Dialects/examples/wist/full-default/dialect.wistdialect");
+if (!dialect.IsSuccess)
 {
-    ["a"] = typeof(int),
-    ["b"] = typeof(int)
-});
-var fastCallable = new DynamicMethodInvoker<int, int, int>(method);
+    Console.WriteLine(dialect.ToDeterministicText());
+    return;
+}
 
-Console.WriteLine("Result: " + fastCallable.Invoke(7, 15));
+using var host = workflow.CreateHost(dialect);
+var result = host.Run("(2 + 2) * 3", "compiler");
+Console.WriteLine(result);
