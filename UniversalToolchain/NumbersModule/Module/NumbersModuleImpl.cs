04eb4d0d0391390eeb31c0822fb014bd56a528f8
Михаил <misha13022008@gmail.com>
2026-03-31 20:14:52 +0300

    Adjust number lexeme pattern to exclude unary sign

--- UniversalToolchain/NumbersModule/Module/NumbersModuleImpl.cs
+++ UniversalToolchain/NumbersModule/Module/NumbersModuleImpl.cs
@@ -9,7 +9,7 @@ public class NumbersModuleImpl : IFrontendCoreModule
     private static readonly IReadOnlyList<LexemeRegistration> _lexemeRegistrations =
     [
         new(
-            @"[+-]?\d+(?:_?\d+)*(?:\.\d+(?:_?\d+)*)?(?:[eE][+-]?\d+(?:_?\d+)*)?",
+            @"\d+(?:_?\d+)*(?:\.\d+(?:_?\d+)*)?(?:[eE][+-]?\d+(?:_?\d+)*)?",
             "Number",
             Priority: -20f
         )
