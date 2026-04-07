0bc2eb55e1ce85dbf4313066ec292de3a9ff17cd
Михаил <misha13022008@gmail.com>
2026-04-07 15:55:35 +0300

    Revert "Introduce explicit CIL intrinsic registry (#153)"
    This reverts commit 95bc9387c6a5a6142b14bc98f14c7d3059eecd20.

--- UniversalToolchain/BytecodeDynamicMethodsCompiler/Compilers/CilIntrinsicDescriptor.cs
+++
@@ -1,7 +0,0 @@
-namespace BytecodeDynamicMethodsCompiler.Compilers;
-
-internal sealed record CilIntrinsicDescriptor(
-    string Name,
-    Action<CompilationContext, Instruction, List<Type>> Compile,
-    Action<Instruction, List<Type>> ProcessTypes
-);
