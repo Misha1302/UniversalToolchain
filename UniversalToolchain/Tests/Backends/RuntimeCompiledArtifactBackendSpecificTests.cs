9cb44f602b78b495341ffb7bbd98f841e316e3ba
Михаил <misha13022008@gmail.com>
2026-04-07 11:37:11 +0300

    Restructure Tests folders and split runtime artifact test responsibilities

---
+++ UniversalToolchain/Tests/Backends/RuntimeCompiledArtifactBackendSpecificTests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Specialized;
+using System.Reflection.Emit;
+using Tests.Infrastructure;
+
+namespace Tests.Backends;
+
+[TestFixture]
+public class RuntimeCompiledArtifactBackendSpecificTests
+{
+    [Test]
+    public void GetArtifactCompiler_WithDynamicMethodOutput_ReturnsWorkingCompilerArtifactPath()
+    {
+        using var host = RuntimeCompiledArtifactTestFactory.CreateHost();
+        var compiler = host.GetArtifactCompiler<DynamicMethod>("compiler");
+        var artifact = compiler.Compile("x", new OrderedDictionary<string, Type> { ["x"] = typeof(object) });
+        var session = artifact.CreateSession();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(artifact.CompilationOutput, Is.Not.Null);
+            Assert.That(artifact.SlotsByName.ContainsKey("x"), Is.True);
+            Assert.That(session, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    public void GetArtifactCompiler_WithMismatchedCompilationOutput_ThrowsInvalidOperationException()
+    {
+        using var host = RuntimeCompiledArtifactTestFactory.CreateHost();
+
+        Assert.Throws<InvalidOperationException>(() => host.GetArtifactCompiler<IAbstractIR>("compiler"));
+    }
+}
---
+++ UniversalToolchain/Tests/Infrastructure/RuntimeCompiledArtifactTestFactory.cs
@@ -0,0 +1,113 @@
+using System.Reflection.Emit;
+using DynamicMethodCalling;
+using ExceptionsManager;
+using Microsoft.Extensions.DependencyInjection;
+using UniversalToolchain.Dialects.Wist;
+
+namespace Tests.Infrastructure;
+
+internal static class RuntimeCompiledArtifactTestFactory
+{
+    public static ICompiledArtifact<DynamicMethod> CreateUnaryAddOneArtifact()
+    {
+        var dynamicMethod = new DynamicMethod("AddOne", typeof(int), [typeof(int)]);
+        var il = dynamicMethod.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Ldc_I4_1);
+        il.Emit(OpCodes.Add);
+        il.Emit(OpCodes.Ret);
+
+        return new CompiledArtifact<DynamicMethod>(
+            "x + 1",
+            [new ExternalBinding { Name = "x", Type = typeof(int), Kind = ExternalBindingKind.Variable }],
+            dynamicMethod,
+            new DynamicMethodExecutor());
+    }
+
+    public static ICompiledArtifact<DynamicMethod> CreateBinaryArtifact()
+    {
+        var dynamicMethod = new DynamicMethod("ConcatDecimalDigits", typeof(int), [typeof(int), typeof(int)]);
+        var il = dynamicMethod.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Ldc_I4_S, 10);
+        il.Emit(OpCodes.Mul);
+        il.Emit(OpCodes.Ldarg_1);
+        il.Emit(OpCodes.Add);
+        il.Emit(OpCodes.Ret);
+
+        return new CompiledArtifact<DynamicMethod>(
+            "x * 10 + y",
+            [
+                new ExternalBinding { Name = "x", Type = typeof(int), Kind = ExternalBindingKind.Variable },
+                new ExternalBinding { Name = "y", Type = typeof(int), Kind = ExternalBindingKind.Variable }
+            ],
+            dynamicMethod,
+            new DynamicMethodExecutor());
+    }
+
+    public static ICompiledArtifact<DynamicMethod> CreateDeclaredOrderArtifact()
+    {
+        var dynamicMethod = new DynamicMethod("DeclaredBindingOrder", typeof(int), [typeof(int), typeof(int)]);
+        var il = dynamicMethod.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Ldc_I4_S, 10);
+        il.Emit(OpCodes.Mul);
+        il.Emit(OpCodes.Ldarg_1);
+        il.Emit(OpCodes.Add);
+        il.Emit(OpCodes.Ret);
+
+        return new CompiledArtifact<DynamicMethod>(
+            "left * 10 + right",
+            [
+                new ExternalBinding { Name = "left", Type = typeof(int), Kind = ExternalBindingKind.Variable },
+                new ExternalBinding { Name = "right", Type = typeof(int), Kind = ExternalBindingKind.Variable }
+            ],
+            dynamicMethod,
+            new DynamicMethodExecutor());
+    }
+
+    public static WistDialectExecutionHost CreateHost()
+    {
+        var services = new ServiceCollection();
+        services.AddWistDialectServices();
+        services.AddWistCilBackend();
+        services.AddWistInterpreterBackend();
+
+        using var provider = services.BuildServiceProvider();
+        var workflow = provider.GetRequiredService<WistDialectExecutionWorkflow>();
+        var composition = workflow.ComposeText(
+            """
+            dialect RuntimeContracts
+            use Whitespaces,SemicolonAsNewLine,Comments,Numbers,Identifier,Arithmetic,Equality,Conditions,Loops,Variables,Scopes,Labels,InternalPreprocessorLexemes,CSharpInterop
+            backend compiler,interpreter
+            """,
+            "runtime-contracts-inline");
+
+        if (!composition.IsSuccess)
+            Thrower.InvalidOpEx(composition.ToDeterministicText());
+
+        return workflow.CreateHost(composition);
+    }
+
+    public static BasicCoreImpl<DynamicMethod> GetCompilerCore(WistDialectExecutionHost host) =>
+        host.GetCore("compiler") as BasicCoreImpl<DynamicMethod>
+        ?? Thrower.InvalidOpEx<BasicCoreImpl<DynamicMethod>>("Compiler core must be BasicCoreImpl<DynamicMethod>.");
+
+    public static BasicCoreImpl<IAbstractIR> GetInterpreterCore(WistDialectExecutionHost host) =>
+        host.GetCore("interpreter") as BasicCoreImpl<IAbstractIR>
+        ?? Thrower.InvalidOpEx<BasicCoreImpl<IAbstractIR>>("Interpreter core must be BasicCoreImpl<IAbstractIR>.");
+
+    private sealed class DynamicMethodExecutor : IExecutor<DynamicMethod>
+    {
+        public object? Execute(DynamicMethod compilation, IExecutionEnvironment environment)
+        {
+            var parameters = compilation.GetParameters();
+            var args = new object?[parameters.Length];
+
+            for (var i = 0; i < parameters.Length; i++)
+                args[i] = environment.GetExternalValue(i);
+
+            return compilation.Invoke(null, args);
+        }
+    }
+}
