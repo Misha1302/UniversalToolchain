212e9dc9fc450c949d73f2e5f72d4472c3ddcdb3
Михаил <misha13022008@gmail.com>
2026-04-08 14:45:13 +0300

    Commit 11 — Remove optimizer semantic side effects

--- UniversalToolchain/DotnetAirHelper/AirTypes.cs
+++ UniversalToolchain/DotnetAirHelper/AirTypes.cs
@@ -2,73 +2,7 @@ namespace DotnetAirHelper;
 
 public static class AirTypes
 {
-    private static readonly Dictionary<string, Action<Instruction, List<Type>>> _intrinsicsProcessors = [];
-    private static bool _defaultsInitialized;
-
-    static AirTypes()
-    {
-        EnsureDefaultsRegistered();
-    }
-
-    private static void EnsureDefaultsRegistered()
-    {
-        if (_defaultsInitialized)
-            return;
-
-        _intrinsicsProcessors.Clear();
-
-        TryRegisterIntrinsic(
-            "call C#",
-            (instruction, stack) =>
-            {
-                var method = instruction.Operands[1].Get<MethodInfo>();
-
-                var methodParams = method.GetParameters().Select(x => x.ParameterType).ToList();
-                var stackTypes = stack.TakeLast(methodParams.Count).ToList();
-                var targetTypes = GenericTypeResolver.GetParameterTypes(method, stackTypes).ToList();
-                if (!method.IsStatic)
-                {
-                    Thrower.AssertAlways(method.DeclaringType != null);
-
-                    targetTypes.Insert(0, method.DeclaringType);
-                    methodParams.Insert(0, method.DeclaringType);
-                    stackTypes.Insert(0, method.DeclaringType);
-                }
-                method = GenericTypeResolver.MakeGenericMethod(method, targetTypes);
-
-                for (var i = 0; i < targetTypes.Count; i++)
-                    stack.Pop();
-                if (method.ReturnType != typeof(void))
-                    stack.Push(method.ReturnType);
-            }
-        );
-        TryRegisterIntrinsic(
-            "call C# ctor",
-            (instruction, stack) =>
-            {
-                var ctor = instruction.Operands[1].Get<ConstructorInfo>();
-                foreach (var _ in ctor.GetParameters()) stack.Pop();
-                stack.Push(ctor.DeclaringType.NotNull());
-            }
-        );
-        TryRegisterIntrinsic(
-            "load_external",
-            (instruction, stack) =>
-            {
-                var valueType = instruction.Operands[2].Get<Type>();
-                stack.Push(valueType);
-            }
-        );
-        TryRegisterIntrinsic(
-            "store_external",
-            (_, stack) =>
-            {
-                stack.Pop();
-            }
-        );
-
-        _defaultsInitialized = true;
-    }
+    private static Dictionary<string, Action<Instruction, List<Type>>> _intrinsicsProcessors = CreateDefaultIntrinsicProcessors();
 
     public static bool TryRegisterIntrinsic(string name, Action<Instruction, List<Type>> processIntrinsic) => _intrinsicsProcessors.TryAdd(name, processIntrinsic);
 
@@ -83,7 +17,52 @@ public static class AirTypes
 
     internal static void ResetToDefaultsForTests()
     {
-        _defaultsInitialized = false;
-        EnsureDefaultsRegistered();
+        _intrinsicsProcessors = CreateDefaultIntrinsicProcessors();
+    }
+
+    private static Dictionary<string, Action<Instruction, List<Type>>> CreateDefaultIntrinsicProcessors()
+    {
+        return new Dictionary<string, Action<Instruction, List<Type>>>
+        {
+            ["call C#"] = (instruction, stack) =>
+            {
+                var method = instruction.Operands[1].Get<MethodInfo>();
+
+                var methodParams = method.GetParameters().Select(x => x.ParameterType).ToList();
+                var stackTypes = stack.TakeLast(methodParams.Count).ToList();
+                var targetTypes = GenericTypeResolver.GetParameterTypes(method, stackTypes).ToList();
+                if (!method.IsStatic)
+                {
+                    Thrower.AssertAlways(method.DeclaringType != null);
+
+                    targetTypes.Insert(0, method.DeclaringType);
+                    methodParams.Insert(0, method.DeclaringType);
+                    stackTypes.Insert(0, method.DeclaringType);
+                }
+
+                method = GenericTypeResolver.MakeGenericMethod(method, targetTypes);
+
+                for (var i = 0; i < targetTypes.Count; i++)
+                    stack.Pop();
+                if (method.ReturnType != typeof(void))
+                    stack.Push(method.ReturnType);
+            },
+            ["call C# ctor"] = (instruction, stack) =>
+            {
+                var ctor = instruction.Operands[1].Get<ConstructorInfo>();
+                foreach (var _ in ctor.GetParameters())
+                    stack.Pop();
+                stack.Push(ctor.DeclaringType.NotNull());
+            },
+            ["load_external"] = (instruction, stack) =>
+            {
+                var valueType = instruction.Operands[2].Get<Type>();
+                stack.Push(valueType);
+            },
+            ["store_external"] = (_, stack) =>
+            {
+                stack.Pop();
+            }
+        };
     }
 }
