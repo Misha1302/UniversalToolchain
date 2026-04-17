60cad2123d5717a41f4f80921afe6dcdf259b31f
Михаил <misha13022008@gmail.com>
2026-04-17 23:20:03 +0300

    Test sandbox added

---
+++ UniversalToolchain/TestSandbox/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExceptionsManager;
+using Microsoft.Extensions.DependencyInjection;
+using UniversalToolchain.Dialects.Wist;
+
+var services = new ServiceCollection();
+
+services.AddWistDialectServices();
+services.AddWistCilBackend();
+services.AddWistInterpreterBackend();
+
+using var provider = services.BuildServiceProvider();
+var workflow = provider.GetRequiredService<WistDialectExecutionWorkflow>();
+var dialect = workflow.ComposeText(
+    """
+    dialect DeclaredBindingsDialect
+    use Arithmetic,Identifier,Numbers,Scopes,Variables,Whitespaces
+    backend compiler,interpreter
+    """,
+    "DeclaredBindingsDialect"
+);
+
+if (!dialect.IsSuccess)
+    Thrower.InvalidOpEx(dialect.ToDeterministicText());
+
+var host = workflow.CreateHost(dialect);
+var result = host.Run("2 + 3", "compiler");
+Console.WriteLine(result ?? "null");
