628e9dd6a113687f58eaa97e2b7dd08d92ebc629
Михаил <misha13022008@gmail.com>
2026-04-25 13:10:26 +0300

    Add neutral function call module global usings

---
+++ UniversalToolchain/FunctionCallsModule/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using BasicCore.Attributes;
+global using BasicCore.Contracts;
+global using BasicCore.Registration;
+global using UniversalToolchain.Capabilities.Abstractions;
+global using UniversalToolchain.Dialects.Abstractions;
