069aeabf154bb669ba461c36638df50af6e00fc3
Михаил <misha13022008@gmail.com>
2026-04-25 23:24:45 +0300

    Wire function calls module lowering

--- UniversalToolchain/FunctionCallsModule/FunctionCallsModuleImpl.cs
+++ UniversalToolchain/FunctionCallsModule/FunctionCallsModuleImpl.cs
@@ -6,8 +6,25 @@ namespace FunctionCallsModule;
 [AutoRegisterService]
 public sealed class FunctionCallsModuleImpl : IFrontendCoreModule
 {
+    private static readonly IReadOnlyList<LexemeRegistration> _lexemeRegistrations =
+    [
+        new(",", "Comma")
+    ];
+
+    public void InitLexer(ILexer lexer) => lexer.AddLexemes(_lexemeRegistrations);
+
     public void InitParser(IParser parser)
     {
         parser.Configuration.NodeCreators.Add(-900, new FunctionCallsNodeCreator());
+    }
+
+    public void InitAstTranslator(IAstToBytecodeTranslator translator, IReadOnlyList<IFrontendCoreModule> selectedModules)
+    {
+        selectedModules = selectedModules.ArgNotNull();
+
+        var capabilityCatalog = new SelectedCapabilityCatalogBuilder()
+            .Build(selectedModules.Select(static x => x.GetType()));
+
+        translator.Configuration.Visitors.Add(new FunctionCallsAstVisitor(capabilityCatalog));
     }
 }
