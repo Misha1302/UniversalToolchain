28c4192cf42d9c6d9998ea0ec7ec543db81b3f45
Михаил <misha13022008@gmail.com>
2026-04-25 22:39:32 +0300

    fix: import parser contracts for function calls module

--- UniversalToolchain/FunctionCallsModule/GlobalUsings.cs
+++ UniversalToolchain/FunctionCallsModule/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using BasicCore.Attributes;
 global using BasicCore.Contracts;
+global using BasicCore.ParserWrapper;
 global using BasicCore.Registration;
 global using UniversalToolchain.Capabilities.Abstractions;
 global using UniversalToolchain.Dialects.Abstractions;
