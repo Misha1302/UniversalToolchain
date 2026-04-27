3a2a636ca7756d84104927991c5efadaf605ab51
Михаил <misha13022008@gmail.com>
2026-04-27 17:30:10 +0300

    remove rule diagnostic codes

--- UniversalToolchain/UniversalToolchain.Diagnostics.Abstractions/ToolchainDiagnosticCodes.cs
+++ UniversalToolchain/UniversalToolchain.Diagnostics.Abstractions/ToolchainDiagnosticCodes.cs
@@ -13,16 +13,4 @@ public static class ToolchainDiagnosticCodes
     public const string TypeMismatch = "UTC-TYPE-001";
 
     public const string CapabilityProviderInvalid = "UTC-CAP-001";
-
-    public const string RuleUnknown = "UTC-RULE-001";
-    public const string RuleArgumentMissing = "UTC-RULE-002";
-    public const string RuleArgumentUnknown = "UTC-RULE-003";
-    public const string RuleArgumentNull = "UTC-RULE-004";
-    public const string RuleArgumentTypeMismatch = "UTC-RULE-005";
-    public const string RuleDuplicateName = "UTC-RULE-006";
-    public const string RuleDuplicateParameter = "UTC-RULE-007";
-    public const string RuleUnknownType = "UTC-RULE-008";
-    public const string RuleInvalidBody = "UTC-RULE-009";
-    public const string RuleDuplicateLocal = "UTC-RULE-010";
-    public const string RuleLocalShadowsParameter = "UTC-RULE-011";
 }
