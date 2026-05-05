c2040537a9490f305c3e495f58a099c15b02a03f
Михаил <misha13022008@gmail.com>
2026-05-06 00:03:24 +0300

    Cover provider-scoped CIL intrinsic registry composition

---
+++ UniversalToolchain/Tests/Backends/CilIntrinsicRegistryCompositionTests.cs
@@ -0,0 +1,28 @@
+using UniversalToolchain.Dialects.Core.ServiceCollection;
+
+namespace Tests.Backends;
+
+[TestFixture]
+public sealed class CilIntrinsicRegistryCompositionTests
+{
+    [Test]
+    public void AddCompilerBackendDefaults_ShouldResolveCompilerWithProviderScopedIntrinsicRegistry()
+    {
+        var services = new ServiceCollection();
+        services.AddCompilerBackendDefaults();
+
+        using var firstProvider = services.BuildServiceProvider();
+        using var secondProvider = services.BuildServiceProvider();
+
+        var firstRegistry = firstProvider.GetRequiredService<CilIntrinsicRegistry>();
+        var secondRegistry = secondProvider.GetRequiredService<CilIntrinsicRegistry>();
+        var compiler = firstProvider.GetRequiredService<AbstractMethodsCompilerImpl>();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(firstProvider.GetRequiredService<CilIntrinsicRegistry>(), Is.SameAs(firstRegistry));
+            Assert.That(secondRegistry, Is.Not.SameAs(firstRegistry));
+            Assert.That(compiler.SupportedIntrinsics, Is.EqualTo(firstRegistry.SupportedIntrinsics));
+        });
+    }
+}
