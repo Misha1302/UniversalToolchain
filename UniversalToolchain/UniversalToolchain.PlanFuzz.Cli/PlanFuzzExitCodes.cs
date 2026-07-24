8f445d3f4a004bf32f6e7b0a51e8440952def406
Михаил <misha13022008@gmail.com>
2026-07-24 23:18:00 +0300

    feat(planfuzz): expose incomplete and flaky outcomes in exit codes

--- UniversalToolchain/UniversalToolchain.PlanFuzz.Cli/PlanFuzzExitCodes.cs
+++ UniversalToolchain/UniversalToolchain.PlanFuzz.Cli/PlanFuzzExitCodes.cs
@@ -8,6 +8,8 @@ public static class PlanFuzzExitCodes
     public const int InfrastructureFailure = 3;
     public const int Finding = 4;
     public const int UnhandledFailure = 5;
+    public const int Inconclusive = 6;
+    public const int Flaky = 7;
     public const int Timeout = 124;
     public const int ProcessCrash = 125;
 }
