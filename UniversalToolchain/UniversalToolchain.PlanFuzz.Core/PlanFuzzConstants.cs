59dd191791c1c5ec101ee05ac72df47a67d79e9b
Михаил <misha13022008@gmail.com>
2026-07-24 23:16:29 +0300

    chore(planfuzz): version the inconclusive replay state

--- UniversalToolchain/UniversalToolchain.PlanFuzz.Core/PlanFuzzConstants.cs
+++ UniversalToolchain/UniversalToolchain.PlanFuzz.Core/PlanFuzzConstants.cs
@@ -7,6 +7,6 @@ public static class PlanFuzzConstants
 {
     public const int CaseSchemaVersion = 1;
     public const int ObservationSchemaVersion = 2;
-    public const int ReplayReportSchemaVersion = 2;
+    public const int ReplayReportSchemaVersion = 3;
     public const string Canonicalization = "planfuzz-json-v1";
 }
