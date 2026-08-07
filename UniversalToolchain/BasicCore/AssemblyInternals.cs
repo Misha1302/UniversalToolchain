99b4da72f005a72c1b6f5a16d7aa438c2d66426f
Михаил <misha13022008@gmail.com>
2026-08-07 21:57:36 +0300

    chore(core): expose canonical stages only to Wist language pack

--- UniversalToolchain/BasicCore/AssemblyInternals.cs
+++ UniversalToolchain/BasicCore/AssemblyInternals.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("BytecodeDynamicMethodsCompiler")]
 [assembly: InternalsVisibleTo("BasicInterpreter")]
 [assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("UniversalToolchain.Wist.LanguagePack")]
