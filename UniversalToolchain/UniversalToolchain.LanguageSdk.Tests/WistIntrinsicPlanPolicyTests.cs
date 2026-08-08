d145a6f5f3350a8c3a394bd291b8d3f63f7d1663
Михаил <misha13022008@gmail.com>
2026-08-08 20:23:14 +0300

    S10: cover intrinsic policy optimizer and backend guards

---
+++ UniversalToolchain/UniversalToolchain.LanguageSdk.Tests/WistIntrinsicPlanPolicyTests.cs
@@ -0,0 +1,66 @@
+using BasicCore.Builtins;
+using BasicCore.Capabilities;
+using UniversalIntermediateRepresentation;
+using UniversalToolchain.FeatureSdk;
+using UniversalToolchain.Language.Abstractions;
+using UniversalToolchain.LanguageSdk;
+using UniversalToolchain.Wist.LanguagePack;
+
+namespace UniversalToolchain.LanguageSdk.Tests;
+
+[TestFixture]
+public sealed class WistIntrinsicPlanPolicyTests
+{
+    private static readonly BackendId Cil = new("cil");
+
+    [Test]
+    public void ForbiddenIntrinsic_IsHiddenFromOptimizerCapabilityContext()
+    {
+        var policy = WistIntrinsicPlanPolicy.Create(CreatePlan(), Cil);
+        var capabilities = policy.ApplyTo(new AllowAllCapabilityContext());
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(capabilities.Supports(BuiltinIntrinsicSymbols.Arithmetic.Add, typeof(int)), Is.False);
+            Assert.That(capabilities.Supports(BuiltinIntrinsicSymbols.Arithmetic.Add, typeof(long)), Is.True);
+        });
+    }
+
+    [Test]
+    public void ForbiddenIntrinsic_ReachingBackendAir_FailsClosed()
+    {
+        var policy = WistIntrinsicPlanPolicy.Create(CreatePlan(), Cil);
+        var air = new AbstractIR();
+        air.AppendInstructions([
+            BuiltinIntrinsicInstruction.Create(BuiltinIntrinsicSymbols.Arithmetic.Add, typeof(int))
+        ]);
+
+        var exception = Assert.Throws<InvalidOperationException>(() => policy.Validate(air));
+
+        Assert.That(exception!.Message, Does.Contain("add_i32").And.Contain("forbidden"));
+    }
+
+    private static LanguagePlan CreatePlan()
+    {
+        var package = new WistLanguageFeaturePackage();
+        var definition = LanguageDefinitionBuilder
+            .Create("wist.intrinsic-policy.tests", WistLanguageFeaturePackage.PackageVersion.Value)
+            .UseRuntimeProvider(WistLanguageFeaturePackage.RuntimeProviderId, WistLanguageFeaturePackage.PackageVersion)
+            .UseFeature(WistFeatureIds.Arithmetic)
+            .UseFeature(WistSsaPolicyFeatureIds.Disabled)
+            .EnableBackend(Cil)
+            .ConfigureIntrinsic(new LanguageIntrinsicId("add_i32"), allowed: false, Cil)
+            .Build();
+
+        return new LanguageCompiler(new LanguagePackageRegistry().AddPackage(package))
+            .Compile(definition)
+            .GetRequiredPlan();
+    }
+
+    private sealed class AllowAllCapabilityContext : IOptimizerIntrinsicCapabilityContext
+    {
+        public bool Supports(IntrinsicSymbol symbol, params Type[] typeArguments) => true;
+
+        public bool Supports(IntrinsicSymbol symbol, IReadOnlyList<Type> typeArguments) => true;
+    }
+}
