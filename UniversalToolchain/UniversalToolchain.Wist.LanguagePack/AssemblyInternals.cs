06173956c83acfc0a10eda220714da9ebe2a3698
Михаил <misha13022008@gmail.com>
2026-08-08 21:05:40 +0300

    S11: allow core tests to exercise canonical Wist internals

--- UniversalToolchain/UniversalToolchain.Wist.LanguagePack/AssemblyInternals.cs
+++ UniversalToolchain/UniversalToolchain.Wist.LanguagePack/AssemblyInternals.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("UniversalToolchain.LanguageSdk.Tests")]
 [assembly: InternalsVisibleTo("UniversalToolchain.Wist")]
 [assembly: InternalsVisibleTo("Wistc")]
+[assembly: InternalsVisibleTo("Tests")]
