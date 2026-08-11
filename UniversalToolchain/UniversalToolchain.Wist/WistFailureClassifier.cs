23603b78d22d1ad1413d205f2fd40618c55b08ef
Михаил <misha13022008@gmail.com>
2026-08-11 14:29:05 +0300

    wist: classify signature and type-resolution failures as user input

--- UniversalToolchain/UniversalToolchain.Wist/WistFailureClassifier.cs
+++ UniversalToolchain/UniversalToolchain.Wist/WistFailureClassifier.cs
@@ -15,9 +15,9 @@ internal static class WistFailureClassifier
             WistResourceLimitException => WistFailureKind.Policy,
             WistDialectFeatureException => WistFailureKind.UserInput,
             LexerException or ParserException or TypeSystemException or ImportException => WistFailureKind.UserInput,
-            AmbiguousMatchException => WistFailureKind.UserInput,
+            ArgumentException or AmbiguousMatchException or TypeLoadException => WistFailureKind.UserInput,
             SsaRouteException => WistFailureKind.Unsupported,
-            TypeLoadException or FileLoadException or FileNotFoundException or IOException or UnauthorizedAccessException =>
+            FileLoadException or FileNotFoundException or IOException or UnauthorizedAccessException =>
                 WistFailureKind.Infrastructure,
             InternalCompilerException or BytecodeGenerationException or RuntimeExecutionException => WistFailureKind.Internal,
             _ => WistFailureKind.Internal
