2f12549f8a3f1b1de7cee4e41a09138ed2057b1a
Михаил <misha13022008@gmail.com>
2026-08-15 14:44:16 +0300

    refactor: remove Wist friend edges from Runtime

--- UniversalToolchain/UniversalToolchain.Runtime/AssemblyInternals.cs
+++ UniversalToolchain/UniversalToolchain.Runtime/AssemblyInternals.cs
@@ -1,6 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("UniversalToolchain.Wist")]
 [assembly: InternalsVisibleTo("UniversalToolchain.Runtime.Tests")]
-[assembly: InternalsVisibleTo("UniversalToolchain.Wist.LanguagePack")]
 [assembly: InternalsVisibleTo("UniversalToolchain.LanguageSdk.Tests")]
